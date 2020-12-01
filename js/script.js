47b718f2068ee5517c3951dbe1a976fe607626c8
Riki Septiadi Maja <32445714+rikismaja@users.noreply.github.com>
2020-12-02 01:29:37 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Network Engineering', 'IT Trainer', 'Co-Ass Lecture'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 350);
+
+})();
